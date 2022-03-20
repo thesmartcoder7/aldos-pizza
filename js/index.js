7f59efb7d2568abd2a6dae238d6aae065aad9043
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-03-20 16:20:03 +0300

    test: add hide and unhide toggle js to the location input

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 let footerYear = document.querySelector(".current-year")
 let currentYear = new Date
+let orderModal = document.querySelector("#order-modal")
+let orderModalHide = document.querySelector("#close-order-modal")
+let orderModalShow = document.querySelector("#order")
+let deliveryChecked = document.querySelector("#delivery-option")
+let locationInput = document.querySelector("#location")
+
+orderModalHide.addEventListener("click", ()=>{
+    orderModal.style.display = "none"
+})
+
+orderModalShow.addEventListener("click", ()=>{
+    orderModal.style.display = "flex"
+})
+
+deliveryChecked.addEventListener("click", ()=>{
+    locationInput.classList.toggle("hide")
+})
 
 footerYear.innerHTML = currentYear.getFullYear()
