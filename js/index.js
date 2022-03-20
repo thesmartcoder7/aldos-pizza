bbf08cabdc559fb18cff843ea1c1a95a10c42b99
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-03-20 17:50:24 +0300

    style: include the confirmation prompts and user journey UI

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ let orderModalHide = document.querySelector("#close-order-modal")
 let orderModalShow = document.querySelector("#order")
 let deliveryChecked = document.querySelector("#delivery-option")
 let locationInput = document.querySelector("#location")
+let orderPlacement = document.querySelector("#place-order")
+let orderTotalPrompt = document.querySelector("#total")
+let orderConfirmation = document.querySelector("#confirm")
+let orderCompletionModal = document.querySelector("#confirmation")
+let orderCompletion = document.querySelector("#order-confirm")
 
 orderModalHide.addEventListener("click", ()=>{
     orderModal.style.display = "none"
@@ -16,6 +21,20 @@ orderModalShow.addEventListener("click", ()=>{
 
 deliveryChecked.addEventListener("click", ()=>{
     locationInput.classList.toggle("hide")
+})
+
+orderPlacement.addEventListener("click", ()=>{
+    orderTotalPrompt.style.display = "block"
+})
+
+orderConfirmation.addEventListener("click", ()=>{
+    orderModal.style.display = "none"
+    orderCompletionModal.style.display = "flex"
+})
+
+orderCompletion.addEventListener("click", ()=>{
+    orderCompletionModal.style.display = "none"
+    window.location.reload()
 })
 
 footerYear.innerHTML = currentYear.getFullYear()
